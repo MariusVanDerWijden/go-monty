d4bc0bfb1ee435d2b524ee770529abbc9c1b3931
Marius van der Wijden <m.vanderwijden@live.de>
2019-09-07 13:16:10 +0200

    fixed monty after review

--- monty.go
+++ monty.go
@@ -1,36 +1,47 @@
 package monty
 
-const SIZE uint32 = 1
+import "os"
 
-func monty(a, b, rInv, N []uint32) []uint32 {
-	var result [SIZE + 2]uint32
-	var temp uint64
-	for i := 0; i < len(a); i++ {
-		var carry uint32
-		for j := 0; j < len(a); j++ {
-			temp = uint64(result[j])
-			temp += uint64(a[i]) * uint64(b[j])
-			temp += uint64(carry)
-			result[j] = uint32(temp)
-			carry = uint32(temp >> 32)
-		}
-		//temp = uint64(result[len(a)]) + uint64(carry)
-		result[len(a)] = uint32(temp)
-		result[len(a)+1] = uint32(temp >> 32)
+const SIZE int = 8
 
-		m := uint64(result[0]) * uint64(rInv[0]) % 32
-		temp = uint64(result[0]) + m*uint64(N[0])
-		carry = uint32(temp >> 32)
-		for k := 1; k < len(a); k++ {
-			temp = uint64(result[k])
-			temp += m * uint64(N[k])
-			temp += uint64(carry)
-			result[k-1] = uint32(temp)
-			carry = uint32(temp >> 32)
-		}
-		temp = uint64(result[len(a)]) + uint64(carry)
-		result[len(a)-1] = uint32(temp)
-		result[len(a)] = result[len(a)+1] + uint32(temp>>32)
+func monty(x, y, montyPrime, modulus []uint32) []uint32 {
+	var N = len(x)
+	if SIZE != N {
+		os.Exit(1)
 	}
-	return result[:]
+	var t [SIZE + 2]uint32
+	var temp uint64
+	for i := 0; i < N; i++ {
+		var carry uint32
+		carry = 0
+		for j := 0; j < N; j++ {
+			temp = uint64(t[j])
+			temp += uint64(x[i]) * uint64(y[j])
+			temp += uint64(carry)
+			t[j] = uint32(temp)
+			carry = uint32(temp >> 32)
+		}
+		temp = uint64(t[N]) + uint64(carry)
+		t[N] = uint32(temp)
+		t[N+1] = uint32(temp >> 32)
+
+		m := uint32(uint64(t[0]) * uint64(montyPrime[0]))
+		temp = uint64(t[0]) + uint64(m)*uint64(modulus[0])
+		carry = uint32(temp >> 32)
+		for k := 1; k < N; k++ {
+			temp = uint64(t[k])
+			temp += uint64(m) * uint64(modulus[k])
+			temp += uint64(carry)
+			t[k-1] = uint32(temp)
+			carry = uint32(temp >> 32)
+		}
+		temp = uint64(t[N]) + uint64(carry)
+		t[N-1] = uint32(temp)
+		t[N] = uint32(temp >> 32)
+	}
+	var result []uint32
+	for i := 0; i < N; i++ {
+		result = append(result, t[i])
+	}
+	return result
 }
